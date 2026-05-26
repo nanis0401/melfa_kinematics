5c70762f63b48cdf8e0e43c69e5c91b4e1edf08a
nanis0401 <julianitadani@hotmail.com>
2026-05-26 18:18:16 -0500

    Update kinematics.py

--- src/kinematics.py
+++ src/kinematics.py
@@ -3,15 +3,10 @@ from src.transformations import Rx, Ry, Rz, Trans
 
 
 def T_base(x, y, z):
-    """Base transformation (translation only)"""
     return Trans(x, y, z)
 
 
 def T_joint(rx, ry, rz, x, y, z):
-    """
-    General homogeneous transformation:
-    rotation + translation
-    """
     R = Rz(rz) @ Ry(ry) @ Rx(rx)
     T = Trans(x, y, z)
     return R @ T
