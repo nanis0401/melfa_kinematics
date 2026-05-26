8e54c2d382b40fcb7d3c0489c8002fbb13dff7d5
nanis0401 <julianitadani@hotmail.com>
2026-05-26 18:29:27 -0500

    Update kinematics.py

--- src/kinematics.py
+++ src/kinematics.py
@@ -10,3 +10,7 @@ def T_joint(rx, ry, rz, x, y, z):
     R = Rz(rz) @ Ry(ry) @ Rx(rx)
     T = Trans(x, y, z)
     return R @ T
+
+if __name__ == "__main__":
+    from src.transformations import Trans
+    print(Trans(1,2,3))
