c6b10af9f6594becfdb871dbe0304b3e7c224888
nanis0401 <julianitadani@hotmail.com>
2026-05-26 18:04:39 -0500

    Update transformations.py
    :(

--- src/transformations.py
+++ src/transformations.py
@@ -1,13 +1,31 @@
 import numpy as np
 
-def Trans(x, y, z):
+
+def Rx(theta):
     return np.array([
-        [1, 0, 0, x],
-        [0, 1, 0, y],
-        [0, 0, 1, z],
-        [0, 0, 0, 1]
+        [1, 0, 0],
+        [0, np.cos(theta), -np.sin(theta)],
+        [0, np.sin(theta), np.cos(theta)]
     ])
-    def Trans(x, y, z):
+
+
+def Ry(theta):
+    return np.array([
+        [np.cos(theta), 0, np.sin(theta)],
+        [0, 1, 0],
+        [-np.sin(theta), 0, np.cos(theta)]
+    ])
+
+
+def Rz(theta):
+    return np.array([
+        [np.cos(theta), -np.sin(theta), 0],
+        [np.sin(theta), np.cos(theta), 0],
+        [0, 0, 1]
+    ])
+
+
+def Trans(x, y, z):
     return np.array([
         [1, 0, 0, x],
         [0, 1, 0, y],
