c8b30bfce0686e9a12b062a61d67e178b1c088b7
nanis0401 <julianitadani@hotmail.com>
2026-05-26 17:35:34 -0500

    Update transformations.py

--- src/transformations.py
+++ src/transformations.py
@@ -32,4 +32,40 @@ def Rz(theta):
         [np.sin(theta), np.cos(theta), 0],
         [0, 0, 1]
     ])
+
+import numpy as np
+
+
+def Rx(theta):
+    """
+    Rotation matrix around X axis.
+    """
+    return np.array([
+        [1, 0, 0],
+        [0, np.cos(theta), -np.sin(theta)],
+        [0, np.sin(theta), np.cos(theta)]
+    ])
+
+
+def Ry(theta):
+    """
+    Rotation matrix around Y axis.
+    """
+    return np.array([
+        [np.cos(theta), 0, np.sin(theta)],
+        [0, 1, 0],
+        [-np.sin(theta), 0, np.cos(theta)]
+    ])
+
+
+def Rz(theta):
+    """
+    Rotation matrix around Z axis.
+    """
+    return np.array([
+        [np.cos(theta), -np.sin(theta), 0],
+        [np.sin(theta), np.cos(theta), 0],
+        [0, 0, 1]
+    ])
+    
     
